ba847c7bd398d0b430a8433611744460af319618
James Turner <jturner@gemini.edu>
2025-12-03 18:14:58 -0300

    Merge my changes/patches from Astroconda ds9 into the new conda-forge recipe.   Patch upstream source to build for X11 on MacOS (using conda-forge libs).   Link correctly against the conda-forge X11 stack (+ scalable fonts) on Linux. Allow for libxml2-devel being split out from libxml2 on conda-forge. Include complete dependencies. Reorganize requirements slightly to eliminate various conda-build warnings. Include associated xpa utilities in the conda package.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,16 +2,33 @@
 
 set -eu
 
-if [[ "$target_platform" == linux-* ]]; then
-  ./unix/configure
-  OUTPUT="bin/ds9"
+if [[ "$target_platform" == osx-* ]]; then
+  LDFLAGS='-arch x86_64 -m64'
+
+  # Needed for conda-forge clang 20 to build some source bundled in v8.6:
+  export CFLAGS="-std=gnu11 ${CFLAGS}"
+  export CXXFLAGS="-std=gnu++11 ${CXXFLAGS}"
+
+  sed -i -e '/codesign/d' ds9/unix/Makefile.in
 
 else
-  ./macos/configure
-  OUTPUT="bin/SAOImageDS9.app"
+  LDFLAGS='-m64'  # still needed?
 
 fi
 
+# Patches are applied in meta.yaml so that everything can be built against
+# conda's X11 libs and the latest ds9 Makefile won't override TKFLAGS.
+#
+# Also dynamically patch several Makefiles that dubiously pass CFLAGS instead
+# of CXXFLAGS to the C++ compiler (for historical reasons), causing failures
+# when C++ therefore gets passed the C "-std" option above:
+find . -name "Makefile.in" -exec sed -ie '/COMPILE_CXX[[:space:]]*=/s|\$(CFLAGS)|\$(CXXFLAGS)|g' {} \;
+
+# Needed to find xorgproto (which replaces xproto) during the build:
+PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${PREFIX}/share/pkgconfig
+
+# Build for X11 (irrespective of the platform):
+unix/configure --x-includes=${PREFIX}/include --x-libraries=${PREFIX}/lib TKFLAGS="--disable-xss"  # not all machines have libXss
 make
 mkdir -p $PREFIX/bin
-cp $OUTPUT $PREFIX/bin/ds9
+cp -a bin/ds9* bin/x* $PREFIX/bin
