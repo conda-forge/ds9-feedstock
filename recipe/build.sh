574682667ddf6cfb70dd44d3bc0d83afc6703749
James Turner <jturner@gemini.edu>
2025-12-05 12:36:37 -0300

    Apply linting suggestions from Google AI / tidy up:
      Undo splitting of run-time deps into a separate output section, as the warning this eliminates is reportedly harmless and the single `outputs` pattern has been known to lead to run_exports breakage.
      Restore make dependency, for consistency.
      Invoke test command in a way that won't break with no DISPLAY.
      Add recommended license_family metadata.
    
      Remove old LDFLAGS settings (which could break future cross-compilation etc.).
      Fix code signing on MacOS (TBC), in preparation for an ARM64 build.
      Build in parallel.
      Quote strings containing variables, to be safe.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,32 +3,37 @@
 set -eu
 
 if [[ "$target_platform" == osx-* ]]; then
-  LDFLAGS='-arch x86_64 -m64'
-
   # Needed for conda-forge clang 20 to build some source bundled in v8.6:
   export CFLAGS="-std=gnu11 ${CFLAGS}"
   export CXXFLAGS="-std=gnu++11 ${CXXFLAGS}"
 
+  # Remove internal codesign; we will do it manually at the end:
   sed -i -e '/codesign/d' ds9/unix/Makefile.in
-
-else
-  LDFLAGS='-m64'  # still needed?
-
 fi
 
-# Patches are applied in meta.yaml so that everything can be built against
-# conda's X11 libs and the latest ds9 Makefile won't override TKFLAGS.
-#
-# Also dynamically patch several Makefiles that dubiously pass CFLAGS instead
-# of CXXFLAGS to the C++ compiler (for historical reasons), causing failures
-# when C++ therefore gets passed the C "-std" option above:
+# Dynamically patch several Makefiles that dubiously pass CFLAGS instead of
+# CXXFLAGS to the C++ compiler (for historical reasons), causing failures when
+# C++ therefore gets passed the C "-std" option above:
 find . -name "Makefile.in" -exec sed -ie '/COMPILE_CXX[[:space:]]*=/s|\$(CFLAGS)|\$(CXXFLAGS)|g' {} \;
 
 # Needed to find xorgproto (which replaces xproto) during the build:
-PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${PREFIX}/share/pkgconfig
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${PREFIX}/share/pkgconfig"
+
+# Patches are applied in meta.yaml so that everything can be built against
+# conda's X11 libs and the latest ds9 Makefile won't override TKFLAGS below.
 
 # Build for X11 (irrespective of the platform):
-unix/configure --x-includes=${PREFIX}/include --x-libraries=${PREFIX}/lib TKFLAGS="--disable-xss"  # not all machines have libXss
-make
-mkdir -p $PREFIX/bin
-cp -a bin/ds9* bin/x* $PREFIX/bin
+./unix/configure \
+  --prefix="${PREFIX}" \
+  --x-includes="${PREFIX}/include" \
+  --x-libraries="${PREFIX}/lib" \
+  TKFLAGS="--disable-xss"  # not really needed & sometimes unavailable
+
+make -j${CPU_COUNT}
+
+mkdir -p "$PREFIX/bin"
+cp -a bin/ds9* bin/x* "$PREFIX/bin/"
+
+if [[ "$target_platform" == osx-* ]]; then
+  codesign -s - -f "${PREFIX}/bin/ds9"
+fi
